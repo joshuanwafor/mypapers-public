8a9926098ddd0abe387557b7959dad718cb3fa77
joshuanwafor <joshuanwafor01@gmail.com>
2021-12-01 13:16:45 +0100

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { SearchComponent } from '../components/search-component';
 export default function Index({ departments }) {
   return (
     <>
+      <OverlayWelcom />
       <MyNavbar></MyNavbar>
       <div id="cover">
         <div className="container-fluid py-5 px-md-5">
@@ -32,7 +33,7 @@ export default function Index({ departments }) {
       <div className="container my-5">
         <h3>Search project by departments</h3>
         <div className="row">
-          {departments.sort((a,b)=>  a.name.localeCompare(b.name)).map(v => {
+          {departments.sort((a, b) => a.name.localeCompare(b.name)).map(v => {
             return <div className="col-12 col-md-4 my-2">
               <Link href={"/projects?department_id=" + v.id}>
                 <a className="text-decoration-none text-body">
@@ -57,4 +58,32 @@ export async function getStaticProps() {
       departments: data
     }
   }
+}
+
+
+export function OverlayWelcom() {
+  return <div style={{ position: "fixed", width: "100%", zIndex: 100000000000000000,background:"rgba(200,200,200,.9)" }}>
+
+    <div className="container p-2" style={{height:"100vh"}}>
+      <div className="row justify-content-center align-items-center " style={{ }}>
+        <div style={{ borderRadius: 10, overflow: "hidden" }} className="cool-10 col-md-6 p-0 text-center bg-light shadow">
+          <div className="mb-2" style={{ paddingTop: '40%' }} id="welcomecover">
+
+          </div>
+          <div className="p-2">
+            <div className="h1 fw-bold">
+              We're building a market place for "academic" research works
+            </div>
+            <p className="lead">Create and Settup an account to published project[s] you'd love to exchange for some cash...</p>
+            <a className="btn btn-lg btn-danger" href="https://app.mypapers.shop/profile">Create Publisher Account</a>
+
+           <p>
+           <small className="my-2 muted">Resources would be open for sale starting from  <b>Jan.01.2022</b></small>
+           </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 }
